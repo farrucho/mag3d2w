a3c7d984fa84f934ac8eb0f85b242339bbceaeb5
farrucho <laranjofclash@gmail.com>
2026-07-10 13:58:34 +0100

    aspas

--- magnet_main.py
+++ magnet_main.py
@@ -64,11 +64,11 @@ def parse_args():
     
     #if the starting position is out of bounds, when the coil is vertical
     if (args.coil_angle != 0) and (args.start_position < 847 or args.start_position > 3500):
-        parser.error("--max_position must be >= 847 when coil angle is not 0 or < 3500”)
+        parser.error("--max_position must be >= 847 when coil angle is not 0 or < 3500")
 
     #if the max position is out of bounds, when the coil is vertical
     if (args.coil_angle != 0) and (args.max_position < 847 or args.max_position > 3500):
-        parser.error("--max_position must be >= 847 when coil angle is not 0 or < 3500”)
+        parser.error("--max_position must be >= 847 when coil angle is not 0 or < 3500")
 	
     #note that max_position < starting_position, this means that the kart moves towards the coil
     #when starting_position < max_position, then the kart moves away from the coil
